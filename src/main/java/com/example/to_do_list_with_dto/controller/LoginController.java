4e064757227f29a02e01f265ae32437537fc9839
nummer14 <nummer14@naver.com>
2025-07-10 16:33:53 +0900

    to do list user

--- src/main/java/com/example/to_do_list_with_dto/controller/LoginController.java
+++ src/main/java/com/example/to_do_list_with_dto/controller/LoginController.java
@@ -27,7 +27,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public String doLogin(
-            @Valid @ModelAttribute("loginDto") LoginDto loginDto,
+            @Valid @ModelAttribute LoginDto loginDto,
             BindingResult bindingResult,
             HttpSession httpSession,
             Model model
--- src/main/java/com/example/to_do_list_with_dto/controller/TodoController.java
+++ src/main/java/com/example/to_do_list_with_dto/controller/TodoController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -67,6 +64,58 @@ public class TodoController {
                 .build();
 
         todoRepository.save(todo);
+
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(
+            @PathVariable int id,
+            Model model,
+            HttpSession httpSession
+    ) {
+        User user = getCurrentUser(httpSession);
+
+        if (user == null) return "redirect:/login";
+
+        Todo todo = todoRepository.findByIdAndUserId(id, user.getId());
+        TodoDto dto = new TodoDto();
+        dto.setId(todo.getId());
+        dto.setTitle(todo.getTitle());
+        dto.setCompleted(todo.isCompleted());
+
+        model.addAttribute("todoDto", dto);
+
+        return "todo-form";
+    }
+
+    @PostMapping("/edit")
+    public String edit(
+            @Valid @ModelAttribute TodoDto todoDto,
+            BindingResult bindingResult,
+            HttpSession httpSession
+    ) {
+        if(bindingResult.hasErrors()) return "todo-form";
+
+        User user = getCurrentUser(httpSession);
+        Todo todo = Todo.builder()
+                .id(todoDto.getId())
+                .title(todoDto.getTitle())
+                .completed(todoDto.isCompleted())
+                .userId(user.getId()).build();
+
+        todoRepository.update(todo);
+
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(
+            @PathVariable int id,
+            HttpSession httpSession
+    ) {
+        User user = getCurrentUser(httpSession);
+        todoRepository.deleteByIdAndUserId(id, user.getId());
 
         return "redirect:/todos";
     }
